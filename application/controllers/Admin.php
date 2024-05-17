1f63628ae30ba5c64b730f26e85d7a79a5888c89
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2024-05-17 14:24:42 +0700

    controller backend

---
+++ application/controllers/Admin.php
@@ -0,0 +1,43 @@
+<?php
+
+
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Admin extends CI_Controller
+{
+
+	public function __construct()
+	{
+		parent::__construct();
+		//Load Dependencies
+
+	}
+
+	// List all your items
+	public function index()
+	{
+		$data = array(
+			'title' => 'Dashboard',
+			'isi' => 'backend/v_backend'
+		);
+		$this->load->view('backend/v_wrapper', $data, FALSE);
+	}
+
+	// Add a new item
+	public function add()
+	{
+	}
+
+	//Update one item
+	public function update($id = NULL)
+	{
+	}
+
+	//Delete one item
+	public function delete($id = NULL)
+	{
+	}
+}
+
+/* End of file Admin.php */
